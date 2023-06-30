25078fb27fa2ac9010d30019307b5e791b9713d2
Kaan Tetik <kaan.tk28@gmail.com>
2023-06-30 21:31:02 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const APIKey = "07f9612354a4fe8fec20e233";
+const url = "https://v6.exchangerate-api.com/v6/" + APIKey;
+
+fetch(url + "/codes")
+.then(response => response.json())
+.then(data => {
+    const items = data.supported_codes;
+    let options;
+
+    for(let item of items) {
+        options += `<option value=${item[0]}>${item[1]}</option>`; 
+    }
+
+    document.getElementById('list_one').innerHTML = options;
+    document.getElementById('list_two').innerHTML = options;
+})
+
+document.getElementById('btnCalculate').addEventListener('click', () => {
+    let currencyOne = document.getElementById('currency_one').value;
+    let currencyTwo = document.getElementById('currency_two').value;
+    let amount = document.getElementById('amount').value;
+
+    fetch(url + "/latest/" + currencyOne)
+    .then(response => response.json())
+    .then(data => {
+
+        const result = data.conversion_rates[currencyTwo] * amount;
+
+        document.getElementById('result').innerHTML = `
+            <p>${amount} USD = ${result} ${currencyTwo}</p>
+        `
+
+    });
+
+})
